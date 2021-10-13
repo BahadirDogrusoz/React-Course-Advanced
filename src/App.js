0abfa2c4388eaf8f22f5c2f8db79d9d323342f80
Mustafa Bahadır Doğrusöz <62994896+BahadirDogrusoz@users.noreply.github.com>
2021-10-13 11:20:00 +0300

    Second Commit

--- src/App.js
+++ src/App.js
@@ -3,25 +3,25 @@ import { Route, Switch } from 'react-router-dom';
 import AllMeetupsPage from "./pages/AllMeetups";
 import NewMeetupPage from "./pages/NewMeetup";
 import FavoritesPage from "./pages/Favorites";
-import MainNavigation from "./components/layout/MainNavigation";
+import Layout from "./components/layout/Layout";
 
 function App() {
-  return( <div>
-    <MainNavigation/>
-    <Switch>
-    <Route path={'/'} exact>
-      <AllMeetupsPage/>
-    </Route>
+  return(
+    <Layout>
+      <Switch>
+      <Route path={'/'} exact>
+        <AllMeetupsPage/>
+      </Route>
 
-    <Route path={'/new-meetup'}>
-      <NewMeetupPage/>
-    </Route>
+      <Route path={'/new-meetup'}>
+        <NewMeetupPage/>
+      </Route>
 
-    <Route path={'/favorites'}>
-      <FavoritesPage/>
-    </Route>
-    </Switch>
-  </div>
+      <Route path={'/favorites'}>
+        <FavoritesPage/>
+      </Route>
+      </Switch>
+    </Layout>
   );
 }
 
--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,6 +1,22 @@
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+
 function NewMeetupPage(){
+    function addMeetupHandler(meetupData){
+        fetch('https://react-getting-started-340f2-default-rtdb.firebaseio.com/meetups.json',{
+            method:'POST',
+            body: JSON.stringify(meetupData),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+        );
+    }
+
     return (
-        <div>New Meetup Page</div>
+        <section>
+            <h1>Add New Meetup</h1>
+            <NewMeetupForm onAddMeetup={addMeetupHandler}/>
+        </section>
     )
 }
 export default NewMeetupPage;
